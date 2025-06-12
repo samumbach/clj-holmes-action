6b8fee17a2630c85147260b6c5a4dca88a827de2
Sam Umbach <samumbach@gmail.com>
2025-06-12 12:30:59 -0400

    WIP add GitHub Action inputs for additional clj-holmes scan options

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,10 @@ output_file="$3"
 fail_on_result="$4"
 verbose="$5"
 path="$6"
+IFS=, read -ra ignored_paths   <<<"$7"
+IFS=, read -ra rule_tags       <<<"$8"
+IFS=, read -ra rule_severities <<<"$9"
+IFS=, read -ra rule_precisions <<<"$10"
 
 if [[ -d "${path}" ]]; then
   cd "${path}"
@@ -32,6 +36,22 @@ if [[ $output_type == "stdout" ]]; then
 else
   SCAN_ARGS+=(-t "$output_type" -o "$output_file")
 fi
+
+for x in "${ignored_paths[@]}"; do
+  SCAN_ARGS+=(--ignored-paths "$x")
+done
+
+for x in "${rule_tags[@]}"; do
+  SCAN_ARGS+=(--rule-tags "$x")
+done
+
+for x in "${rule_severities[@]}"; do
+  SCAN_ARGS+=(--rule-severity "$x")
+done
+
+for x in "${rule_precisions[@]}"; do
+  SCAN_ARGS+=(--rule-precision "$x")
+done
 
 echo "SCAN_ARGS:"
 printf -- "- #%s#\n" "${SCAN_ARGS[@]}"
